611dc85d91567344469e4871cb9cc645556e34ab
Drew-Klepfer <klepfed@wwu.edu>
2022-05-02 19:55:13 -0700

    framework for admin page

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public/assets'));
@@ -31,6 +31,34 @@ app.use(express.static('uploads'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+/* admin page route */
+app.get('/admin.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/admin.html'));
+});
+
+/* get info from admin form */
+app.get('/admin', (req, res) => {
+  var project = req.query.project;
+  var prompt = req.query.prompt;
+  console.log(project, prompt);//testing
+
+  createNewTable(project, prompt);
+});
+
+/* create new collection */
+function createNewTable(project, prompt){
+  /* working, however will crash when you attempt to create a collection that already exists */
+  MongoClient.connect(url, function(err, db) {
+    if(err) throw err;
+    var dbase = db.db("local");
+    dbase.createCollection(project, function(err,res) {
+      if(err) throw err;
+      console.log("created collection");
+      db.close();
+    });
+  });
+}
 
 app.post('/record', upload.single('audio'), (req, res) => res.json({ success: true }));
 
@@ -53,15 +81,15 @@ MongoClient.connect(url, {
   useUnifiedTopology: true
 }, (err, client) => {
   if (err) {
-      return console.log(err);
+    return console.log(err);
   }
-  app.post("/insert", function(req, res) {
+  app.post("/insert", function (req, res) {
     var title = req.body.title;
     var comments = req.body.comments;
     var prompt = req.body.prompt;
     var project = req.body.project;
     var postCode = req.body.postCode;
-    var fullName  = req.body.fullName;
+    var fullName = req.body.fullName;
     var email = req.body.email;
     var phone = req.body.phone;
     const timeStamp = TimeStamp();
@@ -69,14 +97,14 @@ MongoClient.connect(url, {
     console.log(aFile);
     myFunction(title, comments, prompt, project, timeStamp, audio, postCode, fullName, email, phone);
   });
-  function TimeStamp(){
+  function TimeStamp() {
     const currentDate = new Date();
     var year = currentDate.getUTCFullYear();
     var month = currentDate.getUTCMonth();
     var day = currentDate.getUTCDate();
-    var hour = currentDate.getUTCHours() +1;
+    var hour = currentDate.getUTCHours() + 1;
     var minute = currentDate.getUTCMinutes();
-    if (minute < 10){
+    if (minute < 10) {
       minute = "0" + minute;
     }
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -85,36 +113,35 @@ MongoClient.connect(url, {
   // Specify database you want to access
   const db = client.db('local');
   const record = db.collection('recordedData');
-  record.find().toArray(function(err, filed){
-  console.log(filed); // output all records
-  app.post('/url', function(req,res) {
-    return res.json({ success: true, filed });
- });
-  app.post('/saved', function(req,res) {
-    return res.json({ success: true, filed });
- });
+  record.find().toArray(function (err, filed) {
+    console.log(filed); // output all records
+    app.post('/url', function (req, res) {
+      return res.json({ success: true, filed });
+    });
+    app.post('/saved', function (req, res) {
+      return res.json({ success: true, filed });
+    });
 
 
   });
-  function myFunction(title, comments, prompt, project, timeStamp, audio, postCode, fullName, email, phone){
-  record.insertOne({ 
-    adminData:{
-      Project: project,
-      Prompt: prompt,
-      TimeStamp: timeStamp
-    },
-    Audio: {url: audio},
-    metaData: {
-      Title: title,  
-      Comments: comments,
-      PostalCode: postCode,
-      Name: fullName,
-      Email: email,
-      Phone: phone
-    }
-  }, (err, result) => { });
-  console.log(`MongoDB Connected: ${url}`);
+  function myFunction(title, comments, prompt, project, timeStamp, audio, postCode, fullName, email, phone) {
+    record.insertOne({
+      adminData: {
+        Project: project,
+        Prompt: prompt,
+        TimeStamp: timeStamp
+      },
+      Audio: { url: audio },
+      metaData: {
+        Title: title,
+        Comments: comments,
+        PostalCode: postCode,
+        Name: fullName,
+        Email: email,
+        Phone: phone
+      }
+    }, (err, result) => { });
+    console.log(`MongoDB Connected: ${url}`);
   }
-
 
 });
