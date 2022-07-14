041eda366a5d27bb2ebac4eeca9c840faf9a7119
rettusm <rettusm@wwu.edu>
2022-07-14 12:52:49 -0700

    https setup

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require("body-parser")
-
+var enforce = require('express-sslify');
+var http = require('http');
 const multer = require('multer');
 const { ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -57,7 +58,7 @@ app.use(express.static('public/assets'));
 //app.use(express.static('uploads'));
 
 // app.listen(port, () => {
-//     console.log(`App listening at http://localhost:${port}`);
+//     console.log(`App listening at https://localhost:${port}`);
 // });
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
  
