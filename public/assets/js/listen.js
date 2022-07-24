d2d0b3572ba4c5b2b05d67fc065e4f1c817da0e0
behrndc <behrndc@wwu.edu>
2022-07-23 19:42:50 -0700

    moved around some code in listen.js in order to make function calls more straight forward

--- public/assets/js/listen.js
+++ public/assets/js/listen.js
@@ -7,7 +7,9 @@
  */
 const recordingsContainer = document.getElementById('story');
 
-let metaArr = [];
+//run metaGrab on load
+metaGrab();
+
 
 /**
  * Asks /metaArr for required data and 
@@ -19,84 +21,85 @@ function metaGrab() {
         .then((object) => {
             if (object.success && object.filed) {
                 let j = 0;
+                let publicStories = [];
+                //for every element in recieved data, get public items and add to publicStories
                 for (i = 0; i < object.filed.length; i++) {
                     if (object.filed[i].Public) {
-                        metaArr[j] = object.filed[i];
+                        publicStories[j] = object.filed[i];
                         j++;
                     }
                 }
+                return publicStories
             }
+        }).then((object) => {
+            metaData(object)
         })
         .catch((err) => console.error(err));
 }
 
-metaGrab();
-console.log(metaArr);
 
 /**
  * Used for setting up html for display on the listen page
  */
-function metaData() {
+function metaData(publicStories) {
     const project = document.createElement('h4');
-        project.classList.add('metaDataStyle', 'project');
-        const projectNode = document.createTextNode("Project: " + metaArr[0].adminData.Project);
-        project.appendChild(projectNode);
-        recordingsContainer.append(project);
-        /*-----------------------------------------------------------*/
-        const prompt = document.createElement('h6');
-        prompt.classList.add('metaDataStyle', 'prompt');
-        const promptNode = document.createTextNode("Prompt: " + metaArr[0].adminData.Prompt);
-        prompt.appendChild(promptNode);
-        recordingsContainer.append(prompt);
-    for (let m = 0; m < metaArr.length; m++) {
+    project.classList.add('metaDataStyle', 'project');
+    const projectNode = document.createTextNode("Project: " + publicStories[0].adminData.Project);
+    project.appendChild(projectNode);
+    recordingsContainer.append(project);
+    /*-----------------------------------------------------------*/
+    const prompt = document.createElement('h6');
+    prompt.classList.add('metaDataStyle', 'prompt');
+    const promptNode = document.createTextNode("Prompt: " + publicStories[0].adminData.Prompt);
+    prompt.appendChild(promptNode);
+    recordingsContainer.append(prompt);
+    for (let m = 0; m < publicStories.length; m++) {
         const recordingElement = document.createElement('div');
-        if(m === 0){
-            recordingElement.classList.add("slider-item", 'slide-indicator' ,'active');
+        if (m === 0) {
+            recordingElement.classList.add("slider-item", 'slide-indicator', 'active');
         } else {
             recordingElement.classList.add("slider-item", 'slide-indicator');
         }
         recordingElement.setAttribute('id', 'cont' + m)
-        /*-----------------------------------------------------------*/
+            /*-----------------------------------------------------------*/
         const title = document.createElement('p');
         title.classList.add('metaDataStyle');
-        const titleNode = document.createTextNode("* Title: " + metaArr[m].metaData.Title);
+        const titleNode = document.createTextNode("* Title: " + publicStories[m].metaData.Title);
         title.appendChild(titleNode);
         recordingElement.append(title);
         /*-----------------------------------------------------------*/
         const comment = document.createElement('p');
         comment.classList.add('metaDataStyle');
-        const commentNode = document.createTextNode("* Comments: " + metaArr[m].metaData.Comments);
+        const commentNode = document.createTextNode("* Comments: " + publicStories[m].metaData.Comments);
         comment.appendChild(commentNode);
         recordingElement.append(comment);
         /*-----------------------------------------------------------*/
         const timeStamp = document.createElement('p');
         timeStamp.classList.add('metaDataStyle');
-        const timeStampNode = document.createTextNode("* Timestamp: " + metaArr[m].adminData.TimeStamp);
+        const timeStampNode = document.createTextNode("* Timestamp: " + publicStories[m].adminData.TimeStamp);
         timeStamp.appendChild(timeStampNode);
         recordingElement.append(timeStamp);
         /*-----------------------------------------------------------*/
-        if(metaArr[m].metaData.PostalCode !== "")
-        {
+        if (publicStories[m].metaData.PostalCode !== "") {
             const postCode = document.createElement('p');
             postCode.classList.add('metaDataStyle');
-            const postCodeNode = document.createTextNode("* Post Code: " + metaArr[m].metaData.PostalCode);
+            const postCodeNode = document.createTextNode("* Post Code: " + publicStories[m].metaData.PostalCode);
             postCode.appendChild(postCodeNode);
             recordingElement.append(postCode);
         }
         /*---------------------------------------------------------- */
         const audioTag = document.createElement('div');
         audioTag.classList.add("playStory");
-        audioTag.innerHTML = '<audio id="audio-player" controls="controls" src= ' + metaArr[m].Audio.url + ' type="audio/mpeg">';
+        audioTag.innerHTML = '<audio id="audio-player" controls="controls" src= ' + publicStories[m].Audio.url + ' type="audio/mpeg">';
         recordingElement.append(audioTag);
         /*---------------------------------------------------------- */
         const prv = document.getElementById('prv');
         const nxt = document.getElementById('nxt');
+        const pnum = document.getElementById('postNumber');
         recordingsContainer.append(recordingElement);
         recordingsContainer.appendChild(prv);
         recordingsContainer.appendChild(nxt);
     }
 };
 
-setTimeout(() => metaData(), 200);
-
-
+// setTimeout(() => metaData(), 200);
