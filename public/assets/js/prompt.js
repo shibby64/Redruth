7ba8da0279d920506ca5e5ab4c9300b27ef7b049
sparlij <sparlij@wwu.edu>
2023-04-27 15:27:05 -0700

    adding prompt filtering for users

--- public/assets/js/prompt.js
+++ public/assets/js/prompt.js
@@ -1,20 +1,65 @@
-updatePrompt();
+//updatePrompt();
+recordingPrompts();
+getCurrentCollection()
 
-function updatePrompt() {
-    var queryStr = window.location.search;
-    const showP = document.getElementsByClassName('showPrompt');
-    const promptInput = document.getElementById('prompt');
-    fetch('/prompt' + queryStr, {  method: 'GET' })
-        .then(res => res.json())
-        .then(res => {
-            try {
-                Array.from(showP).forEach(element => {
-                    element.innerText = res.prompt;
-                });
-            } catch (error) {
-                console.error(error);
+// function updatePrompt() {
+//     var queryStr = window.location.search;
+//     const showP = document.getElementsByClassName('showPrompt');
+//     const promptInput = document.getElementById('prompt');
+//     fetch('/prompt' + queryStr, {  method: 'GET' })
+//         .then(res => res.json())
+//         .then(res => {
+//             try {
+//                 Array.from(showP).forEach(element => {
+//                     element.innerText = res.prompt;
+//                 });
+//             } catch (error) {
+//                 console.error(error);
+//             }
+//             promptInput.value = res.prompt_id;
+//         });
+// }
+const promptList = [];
+
+function recordingPrompts() {
+    var selection = document.getElementById('recordPrompts');
+    fetch('/prompts' , { method: 'POST' })
+        .then((object) => object.json())
+        .then((prompts) => {
+            for (i = 0; i < prompts.results.length; i++) {
+                const dropdownhtmlList = document.createElement("li");
+                const dropdownhtmlB = document.createElement("button");
+                const dropdownhtmlBA = document.createElement("a");
+                dropdownhtmlB.setAttribute("class", "dropdown-item")
+                dropdownhtmlB.setAttribute("id", "dropdown-styling")
+                dropdownhtmlBA.setAttribute("href", 'localhost:3000/?promptid=' + prompts.results[i].prompt_id)
+                dropdownhtmlBA.innerHTML = prompts.results[i].prompt;
+                dropdownhtmlB.innerHTML = prompts.results[i].prompt;
+                dropdownhtmlList.append(dropdownhtmlB);
+                if (prompts.results[i].prompt_id !== 1) {
+                    selection.append(dropdownhtmlList);
+                }
+                promptList.push(prompts.results[i]);
+                document.getElementById('currentPromptList').innerHTML = 'Prompts';
+                dropdownhtmlB.addEventListener('click', function(event) {
+                    //alert(dropdownhtmlB.innerHTML);
+                    for (var i = 0; i < promptList.length; i++) {
+                        if (promptList[i].prompt === dropdownhtmlB.innerHTML) {
+                            window.location.href = "https://readingroom.herokuapp.com/?promptid=" + promptList[i].prompt_id;
+                            document.getElementById('currentPromptList').innerHTML = promptList[i].prompt;
+                        }
+                    }
+                  })
             }
-            promptInput.value = res.prompt_id;
-        });
+        })
 }
 
+function getCurrentCollection() {
+    var project = document.getElementById('project');
+    fetch('/currentCollectionPrompt', { method: 'GET' })
+        .then((object) => object.json())
+        .then((collection) => {
+            // alert(collection.results[0].title)
+            project.innerHTML = collection.results[0].title;
+        })
+}
