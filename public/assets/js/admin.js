542c1397237ecbcdf925bae31aa8c62981f00d8c
Jack Sparling <sparlij@wwu.edu>
2023-05-17 13:33:14 -0700

    working on demo

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -72,7 +72,8 @@ async function getCurrentCollectionPrompt() {
         
         let promptLinkBtn = document.getElementById("promptLink");
         promptLinkBtn.addEventListener("click", function(event){
-          navigator.clipboard.writeText("https://readingroom.herokuapp.com/?promptid=" + collection.results[0].promptID);
+          navigator.clipboard.writeText("https://readingroom.herokuapp.com/");
+          //navigator.clipboard.writeText("https://readingroom.herokuapp.com/?promptid=" + collection.results[0].promptID);
         });
         if (collection.results[0].isPublic) {
           document.getElementById("collectionPublicBtn").setAttribute("class", "btn btn-outline-success");
